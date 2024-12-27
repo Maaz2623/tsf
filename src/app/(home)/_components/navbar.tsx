c9a121be995cda0e865086611d6f83eaae2a6f5f
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-27 22:51:24 +0530

    modified:   bun.lockb 	new file:   components.json 	modified:   package.json 	new file:   public/banner.jpeg 	new file:   public/dummy-profile-image.svg 	new file:   public/logo.png 	new file:   src/app/(auth)/layout.tsx 	new file:   src/app/(auth)/sign-in/[[...sign-in]]/page.tsx 	new file:   src/app/(auth)/sign-up/[[...sign-up]]/page.tsx 	new file:   src/app/(home)/_components/navbar.tsx 	new file:   src/app/(home)/layout.tsx 	new file:   src/app/(home)/page.tsx 	modified:   src/app/globals.css 	modified:   src/app/layout.tsx 	deleted:    src/app/page.tsx 	new file:   src/components/convex-client-provider.tsx 	new file:   src/components/ui/accordion.tsx 	new file:   src/components/ui/alert-dialog.tsx 	new file:   src/components/ui/alert.tsx 	new file:   src/components/ui/aspect-ratio.tsx 	new file:   src/components/ui/avatar.tsx 	new file:   src/components/ui/badge.tsx 	new file:   src/components/ui/breadcrumb.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/components/ui/calendar.tsx 	new file:   src/components/ui/card.tsx 	new file:   src/components/ui/carousel.tsx 	new file:   src/components/ui/chart.tsx 	new file:   src/components/ui/checkbox.tsx 	new file:   src/components/ui/collapsible.tsx 	new file:   src/components/ui/command.tsx 	new file:   src/components/ui/context-menu.tsx 	new file:   src/components/ui/dialog.tsx 	new file:   src/components/ui/drawer.tsx 	new file:   src/components/ui/dropdown-menu.tsx 	new file:   src/components/ui/form.tsx 	new file:   src/components/ui/hover-card.tsx 	new file:   src/components/ui/input-otp.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/label.tsx 	new file:   src/components/ui/menubar.tsx 	new file:   src/components/ui/navigation-menu.tsx 	new file:   src/components/ui/pagination.tsx 	new file:   src/components/ui/popover.tsx 	new file:   src/components/ui/progress.tsx 	new file:   src/components/ui/radio-group.tsx 	new file:   src/components/ui/resizable.tsx 	new file:   src/components/ui/scroll-area.tsx 	new file:   src/components/ui/select.tsx 	new file:   src/components/ui/separator.tsx 	new file:   src/components/ui/sheet.tsx 	new file:   src/components/ui/sidebar.tsx 	new file:   src/components/ui/skeleton.tsx 	new file:   src/components/ui/slider.tsx 	new file:   src/components/ui/sonner.tsx 	new file:   src/components/ui/switch.tsx 	new file:   src/components/ui/table.tsx 	new file:   src/components/ui/tabs.tsx 	new file:   src/components/ui/textarea.tsx 	new file:   src/components/ui/toast.tsx 	new file:   src/components/ui/toaster.tsx 	new file:   src/components/ui/toggle-group.tsx 	new file:   src/components/ui/toggle.tsx 	new file:   src/components/ui/tooltip.tsx 	new file:   src/hooks/use-mobile.tsx 	new file:   src/hooks/use-toast.ts 	new file:   src/lib/utils.ts 	new file:   src/middleware.ts 	modified:   tailwind.config.ts

---
+++ src/app/(home)/_components/navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Teko } from "next/font/google";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+const teko = Teko({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className="border-gray-150 dark:border-gray-800 h-14 px-4 py-3 flex border-b justify-between items-center">
+      <div className={`flex items-center justify-center gap-x-1`}>
+        <Image
+          src={`/logo.png`}
+          alt="logo"
+          width={100}
+          height={100}
+          className="border size-10"
+        />
+        <p className={`${teko.className} text-xl sm:text-2xl lg:text-3xl`}>
+          The Student Forum
+        </p>
+      </div>
+
+      <div className="flex gap-x-2 items-center">
+        {theme === "light" && (
+          <MoonIcon
+            className="dark:hover:bg-white/5 hover:bg-black/5 cursor-pointer rounded-sm size-8 p-1"
+            onClick={() => setTheme("dark")}
+          />
+        )}
+        {theme === "dark" && (
+          <SunIcon
+            className="dark:hover:bg-white/5 hover:bg-black/5 cursor-pointer size-8 rounded-sm p-1"
+            onClick={() => setTheme("light")}
+          />
+        )}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={`secondary`}>Sign in</Button>
+            </DialogTrigger>
+            <DialogContent className="flex justify-center items-center bg-transparent border-none shadow-none">
+              <DialogHeader>
+                <DialogTitle></DialogTitle>
+                <DialogDescription></DialogDescription>
+              </DialogHeader>
+              <SignIn signUpFallbackRedirectUrl={`/events`} routing="hash" />
+            </DialogContent>
+          </Dialog>
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      afterSignOutUrl={`/`}
+      signInFallbackRedirectUrl={`/events`}
+      signUpFallbackRedirectUrl={`/events`}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`antialiased `}>
+          <ConvexClientProvider>
+            {children} <Toaster />
+          </ConvexClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
