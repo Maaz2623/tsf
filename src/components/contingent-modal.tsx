6427679f8a583e75b395c05ddf5605b00066c16c
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-03-09 08:53:18 +0530

    modified:   src/components/contingent-modal.tsx

--- src/components/contingent-modal.tsx
+++ src/components/contingent-modal.tsx
@@ -35,7 +35,6 @@ const ContingentModal = () => {
 
   const [contingentModalOpen, setContingentModalOpen] = useState(false);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: hasContingent } = trpc.contingents.hasContingent.useQuery();
 
   return (
@@ -51,8 +50,12 @@ const ContingentModal = () => {
         open={contingentModalOpen}
       >
         <button
-          disabled={true}
-          onClick={() => setContingentModalOpen(!contingentModalOpen)}
+          onClick={() => {
+            if (hasContingent) {
+              toast.custom("You already have a package booked");
+            }
+            setContingentModalOpen(!contingentModalOpen);
+          }}
           className="inline-flex h-8 text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 "
         >
           <PackageIcon className="size-5 mr-2" />
