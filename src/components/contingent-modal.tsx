6b2ebd225bfcc24d15995bbf3e441e4a9b3c7b97
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-03-09 09:16:06 +0530

    modified:   src/components/contingent-modal.tsx

--- src/components/contingent-modal.tsx
+++ src/components/contingent-modal.tsx
@@ -72,11 +72,13 @@ const ContingentModal = () => {
           }}
           className="inline-flex h-8 text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 "
         >
-          <PackageIcon className="size-5 mr-2" />
           {isFetching ? (
             <Loader2Icon className="size-4 animate-spin" />
           ) : (
-            "Buy Contingent"
+            <>
+              <PackageIcon className="size-5 mr-2" />
+              Buy Contingent
+            </>
           )}
         </button>
         <AlertDialogContent className="">
