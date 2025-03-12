b3365aab5b659fa14661482804a90066a4c336e4
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-03-12 22:47:09 +0530

    modified:   src/components/contingent-modal.tsx

--- src/components/contingent-modal.tsx
+++ src/components/contingent-modal.tsx
@@ -69,6 +69,7 @@ const ContingentModal = () => {
                   </p>
                 </div>
               ));
+              return;
             }
             if (hasContingent) {
               toast.custom((t) => (
@@ -83,9 +84,10 @@ const ContingentModal = () => {
                   </p>
                 </div>
               ));
-            } else {
-              setContingentModalOpen(!contingentModalOpen);
+              return;
             }
+
+            setContingentModalOpen(!contingentModalOpen);
           }}
           className="inline-flex h-8 text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 "
         >
