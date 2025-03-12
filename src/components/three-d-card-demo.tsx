9da93e7f0d1961473e42a5bcfd71a5ee4f271585
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-03-12 18:00:47 +0530

    modified:   src/components/three-d-card-demo.tsx

--- src/components/three-d-card-demo.tsx
+++ src/components/three-d-card-demo.tsx
@@ -49,8 +49,8 @@ export function ThreeDCardDemo({
       title,
     },
     {
-      staleTime: 5000,
-      refetchInterval: 5000,
+      staleTime: 10000,
+      refetchOnWindowFocus: true,
     }
   );
 
@@ -60,8 +60,8 @@ export function ThreeDCardDemo({
         title,
       },
       {
-        staleTime: 5000,
-        refetchInterval: 5000,
+        staleTime: 10000,
+        refetchOnWindowFocus: true,
       }
     );
 
